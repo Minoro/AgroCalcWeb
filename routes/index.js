077daba576c5a657a82cd2d016947c16c3d75f8f
André Minoro Fusioka <minorofusioka@hotmail.com>
2017-05-13 16:46:53 -0300

    Rotas básicas

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+var homeController = require('../controllers/HomeController');
+
+router.get('/', homeController.index);
+
+
+module.exports = router;
