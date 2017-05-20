d482a0c4aa344601376e36aecd26e93a0371d3bc
André Minoro Fusioka <minorofusioka@hotmail.com>
2017-05-20 20:05:19 -0300

    utiliza arquivos minificados

--- public/dist/js/app/main.js
+++ public/dist/js/app/main.js
@@ -73,3 +73,15 @@ if (typeof CalcPulverizador.calcQtdProdutoTanque == 'function') {
  } else {
      var calcQtdProdutoTanque = function(){return false};
  }
+
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./dist/js/app/service_worker.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+}).catch(function(err) {
+    // registration failed :(
+    	console.log('ServiceWorker registration failed: ', err);
+    });
+}
