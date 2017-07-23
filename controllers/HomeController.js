c03d4c04f0dabcd4a78dc420d9470460dbb9aa4d
André Minoro Fusioka <minorofusioka@hotmail.com>
2017-07-23 16:06:55 -0300

    Novo git ignore

--- controllers/HomeController.js
+++
@@ -1,7 +0,0 @@
-module.exports.index = function(req, res) {
-    res.render('index', {'title': 'AgroCalc Web'});
-}
-
-module.exports.sobre = function(req, res) {
-    res.render('sobre', {'title': 'AgroCalc Web - Sobre'});
-}
